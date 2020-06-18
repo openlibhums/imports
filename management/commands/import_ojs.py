167293bd9cf9badb6724c26dff5e03cd96be726c
Mauro MSL <mauro.msl@outlook.com>
2020-06-18 10:35:58 +0100

    [OJS] Adds editorial only mode

--- management/commands/import_ojs.py
+++ management/commands/import_ojs.py
@@ -16,6 +16,8 @@ class Command(BaseCommand):
         parser.add_argument('journal_code')
         parser.add_argument('--ojs_password', default=None)
         parser.add_argument('--dry-run', action="store_true", default=False)
+        parser.add_argument('--editorial', action="store_true", default=False
+                            help="Imports only content in the editorial flow")
 
     def handle(self, *args, **options):
         journal = models.Journal.objects.get(code=options["journal_code"])
@@ -29,6 +31,9 @@ class Command(BaseCommand):
         )
 
         ojs.import_users(client, journal)
-        ojs.import_articles(client, journal)
-        ojs.import_issues(client, journal)
-        ojs.import_metrics(client, journal)
+        ojs.import_in_review_articles(client, journal)
+        ojs.import_in_editing_articles(client, journal)
+        if not options["editorial"]:
+            ojs.import_published_articles(client, journal)
+            ojs.import_issues(client, journal)
+            ojs.import_metrics(client, journal)
