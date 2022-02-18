242e6b42a916d0d10c3833e3953e75f591875136
mauromsl <mauro.msl@outlook.com>
2022-02-18 12:52:09 +0100

    #22: Adds CLI interface for update_article_metadata

--- management/commands/update_csv_article_metadata.py
+++ management/commands/update_csv_article_metadata.py
@@ -7,7 +7,7 @@ from django.core.management.base import BaseCommand
 from journal import models
 
 from plugins.imports.utils import DummyRequest
-from plugins.imports.utils import import_article_metadata
+from plugins.imports.utils import update_article_metadata
 
 class Command(BaseCommand):
     """ CLI interface for the CSV importer"""
@@ -16,20 +16,18 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('csv_file')
-        parser.add_argument('journal_code')
         parser.add_argument('--owner-id', default=1)
 
     def handle(self, *args, **options):
-        journal = models.Journal.objects.get(code=options["journal_code"])
         owner = Account.objects.get(pk=options["owner_id"])
 
         with open(options["csv_file"], "r") as f:
-            reader = csv.reader(f, delimiter=",")
-            request = DummyRequest(owner, journal)
-            _, err, err_file= import_article_metadata(request, reader)
+            reader = csv.DictReader(f, delimiter=",")
+            errors, actions = update_article_metadata(reader, owner=owner)
 
-            for e in err:
-                print(e)
-            print("Error file: %s" % err_file)
+            for e in errors:
+                self.sys.stderr(e)
+            for action in actions:
+                print(action)
 
 
