93bf5148d5fe7edc7f4766bd8da6c17381fb0fd3
mauromsl <mauro.msl@outlook.com>
2023-03-27 18:55:00 +0200

    Bumps Janeway minimum version to 1.5.0

--- plugin_settings.py
+++ plugin_settings.py
@@ -1,10 +1,10 @@
 PLUGIN_NAME = 'Import Plugin'
 DESCRIPTION = 'This plugin is a collection of import scripts.'
 AUTHOR = 'Birkbeck Centre for Technology and Publishing'
-VERSION = '1.7'
+VERSION = '1.8'
 SHORT_NAME = 'imports'
 MANAGER_URL = 'imports_index'
-JANEWAY_VERSION = "1.4.1"
+JANEWAY_VERSION = "1.5.0"
 
 from utils import models
 
