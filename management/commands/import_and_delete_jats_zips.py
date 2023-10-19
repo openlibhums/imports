510136163d66b7ceb205b3dd1a5b98514cbefa31
Andy Byers <a.byers@bbk.ac.uk>
2023-10-19 12:59:10 +0100

    Adds a new command that will import a series of zip files found in a directory and unlink them after

--- management/commands/import_and_delete_jats_zips.py
+++ management/commands/import_and_delete_jats_zips.py
@@ -1,8 +1,11 @@
+import os
 import pprint
 
 from django.core.management.base import BaseCommand
 from journal import models
 from core.models import Account
+
+from zipfile import BadZipfile
 
 from plugins.imports.jats import import_jats_zipped
 
@@ -13,7 +16,7 @@ class Command(BaseCommand):
     help = "Imports zipped articles in JATS XML Format"
 
     def add_arguments(self, parser):
-        parser.add_argument('zip_file')
+        parser.add_argument('folder')
         parser.add_argument('-j', '--journal_code')
         parser.add_argument('-o', '--owner_id', default=1)
         parser.add_argument('-d', '--dry-run', action="store_true", default=False)
@@ -22,17 +25,27 @@ class Command(BaseCommand):
         journal = None
         if options["journal_code"]:
             journal = models.Journal.objects.get(code=options["journal_code"])
+        folder_path = options.get('folder')
         owner = Account.objects.get(pk=options["owner_id"])
+        zip_files = [
+            os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))
+        ]
         persist = True
         if options["dry_run"]:
             persist = False
-        articles = import_jats_zipped(
-            options["zip_file"], journal,
-            owner=owner, persist=persist,
-        )
-        for article in articles:
-            if not persist:
-                pprint.pprint(article)
-            else:
-                print("Imported Article %s" % article)
 
+        for zip_file in zip_files:
+            try:
+                articles = import_jats_zipped(
+                    zip_file, journal,
+                    owner=owner, persist=persist,
+                )
+                for article in articles:
+                    if not persist:
+                        pprint.pprint(article)
+                    else:
+                        print("Imported Article %s" % article)
+                if persist:
+                    os.unlink(zip_file)
+            except BadZipfile as e:
+                print(f"Error importing: {e}")
