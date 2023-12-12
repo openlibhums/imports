97ac928508191aa6e64ca31a4524443b0492339f
Mauro MSL <mauro.msl@outlook.com>
2023-12-12 09:10:04 +0100

    Bump version v1.9

--- plugin_settings.py
+++ plugin_settings.py
@@ -1,10 +1,10 @@
 PLUGIN_NAME = 'Import Plugin'
 DESCRIPTION = 'This plugin is a collection of import scripts.'
 AUTHOR = 'Birkbeck Centre for Technology and Publishing'
-VERSION = '1.8'
+VERSION = '1.9'
 SHORT_NAME = 'imports'
 MANAGER_URL = 'imports_index'
-JANEWAY_VERSION = "1.5.0"
+JANEWAY_VERSION = "1.5.1"
 
 from utils import models
 
