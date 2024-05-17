e473c46e9d00350d44ebd71c6d13ed63f5dde9a7
Mauro MSL <mauro.msl@outlook.com>
2024-05-17 19:26:13 +0100

    WT: Re-create navigation

--- import_wagtail.py
+++ import_wagtail.py
@@ -1,4 +1,4 @@
-from cms.models import Page
+from cms.models import NavigationItem, Page
 from comms.models import NewsItem
 from django.contrib.contenttypes.models import ContentType
 import json
@@ -11,6 +11,7 @@ from plugins.imports.utils import get_aware_datetime
 logger = get_logger(__name__)
 
 LARGE_INT = 32768
+SITE_PREFIX = "site"
 
 
 def import_from_json(press, json_data, site_prefix=" ", dry_run=False):
@@ -38,6 +39,9 @@ def import_page(press, page_data, site_prefix=" ", dry_run=False):
     if not url:
         return
     content_type = ContentType.objects.get_for_model(press)
+    if url.endswith("/"):
+        # Janeway autoappends trail slash
+        url = url[:-1]
 
     jw_page_data = dict(
         display_name=page_data["content"]["title"],
@@ -55,6 +59,10 @@ def import_page(press, page_data, site_prefix=" ", dry_run=False):
         logger.info("Created page %s", page.name)
     else:
         logger.info("Updated page %s", page.name)
+
+    if page_data["content"].get("show_in_menus"):
+        sequence = LARGE_INT - page_data["content"]["pk"]
+        import_nav_link(press, page, sequence)
 
 
 def import_news_item(press, page_data, site_prefix=" ", dry_run=False):
@@ -78,4 +86,65 @@ def import_news_item(press, page_data, site_prefix=" ", dry_run=False):
         logger.info("Created news item %s", news_item.title)
     else:
         logger.info("Updated news item %s", news_item.title)
+
+def import_nav_link(press, page, sequence):
+    url = page.name
+
+    # /parent/path/to/page/ -> ["", "parent", "path/to/page/"]
+    # /parent/             -> ["", "parent", ""]
+    top_path, *child_path = url.split("/", 1)
+    top_nav,c = update_or_create_nav(
+        press, top_path,
+        prefixes=[SITE_PREFIX],
+        parent=None,
+        title=None if child_path else page.display_name,
+        seq=None if child_path else sequence,
+    )
+    if child_path:
+        # unwrap
+        child_path = child_path[0]
+        top_nav.has_sub_nav = True
+        top_nav.save()
+        child_nav, c = update_or_create_nav(
+            press, child_path,
+            prefixes=[SITE_PREFIX, top_path],
+            parent=top_nav,
+            title=page.display_name,
+            seq=sequence,
+        )
+
+
+def update_or_create_nav(press, path, prefixes=None, parent=None, title=None, seq=None):
+
+    if prefixes:
+        path = "/".join(prefixes + [path])
+
+    if not parent and not path.startswith("/"):
+        path = f"/{path}"
+
+    defaults = {
+        "link_name": title or path, # Field is required
+        "sequence": seq or 0,
+    }
+    if parent:
+        defaults["top_level_nav"] = parent
+
+
+    nav, c = NavigationItem.objects.get_or_create(
+        content_type=ContentType.objects.get_for_model(press),
+        object_id=press.id,
+        link=path,
+        defaults=defaults
+    )
+
+    # Perform update
+    if title:
+        nav.link_name = title
+    if seq:
+        nav.sequence = seq
+
+
+    nav.save()
+    return nav, c
+
 
